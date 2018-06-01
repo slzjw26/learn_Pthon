e8b7ab1db0fabf2938902140ccc57d13cd7bae2b
Moriarty <slzjw26@qq.com>
2018-06-01 16:51:50 +0800

    update

--- D2/d2.2.py
+++ D2/d2.2.py
@@ -4,23 +4,28 @@ from datetime import datetime, timedelta
 
 
 if len(sys.argv) != 3:
-    print('usage: d2.2.py<date time string><add days>')
+    print('usage: d2.2.py<date time string><time delta>')
     exit(1)
 
 time_str = sys.argv[1]
 add_days = sys.argv[2]
 time_format = '%Y-%m-%d %H:%M:%S'
 
+if add_days.startswith('-'):
+    tmp_add = add_days[1:]
+else:
+    tmp_add = add_days
+if not tmp_add.isdigit():
+    print('invalid time delta: %s' % add_days)
+    exit(1)
+
+add_days = int(add_days)
+delta = timedelta(add_days)
+
 try:
     t = time.strptime(time_str, time_format)
-    add_days = int(add_days)
-    delta = timedelta(add_days)
 except ValueError:
-    print('invalid value')
-    # print('invalid time string: %s' % time_str)
-    exit(1)
-except TypeError as e:
-    print('Error: %s' % e)
+    print('invalid time string: %s' % time_str)
     exit(1)
 
 t2 = t[:6]
